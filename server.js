68fc19cdd541cc4ca57919240ba5cbfdd0e9457a
laynoor <layla.landrum@tufts.edu>
2025-04-28 14:37:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const {MongoClient} = require("mongodb")
-const mongourl = "mongodb+srv://new_user:zoo44321@cluster0.d0bwl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const mongourl = "mongodb+srv://new_user:zoo44321@cluster0.d0bwl.mongodb.net/%3FretryWrites=true&w=majority&appName=Cluster0";
 const app = express();
-const client = new MongoClient(mongourl);
+const client = new MongoClient(mongourl,{
+    tls:true
+});
 const db = client.db("Stock");
 const col = db.collection("PublicCompanies");
 app.use(express.json());
@@ -16,10 +18,14 @@ app.get("/query", async (req,res) => {
     res.json({data:foundcompany});
 })
 const run = async() => {
+  try {
     await client.connect();
     app.listen(process.env.PORT || 3000, () => {
         console.log("App is listening.")
     })
+  } catch (error) {
+    console.log(error);
+  }
 }
 run();
 
